86b0bbc25cc542fb2367c9ca3a55c1734b30162c
Eko - Garudaku Developers <46987303+linedestiny7@users.noreply.github.com>
2023-03-29 17:55:37 +0700

    Initial commit

---
+++ markerinfo/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Belajar Info windows</title>
+    <style>
+        /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+
+        /* Optional: Makes the sample page fill the window. */
+        html,
+        body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- elemen untuk menampilkan peta -->
+    <div id="mapreposisi" style="height:100%;"></div>
+
+
+    <script>
+        function initMap() {
+
+
+            // membuat objek peta
+            var mapreposisi = new google.maps.Map(document.getElementById('mapreposisi'), {
+                zoom: 9,
+                center: {
+                    lat: -8.352553338128352,
+                    lng: 115.90582912063248
+                }
+            });
+
+
+            //// DATA 3 //
+            var data2 = {
+                lat: -8.35325789882126,
+                lng: 115.24945007665592
+            };
+
+
+            // membuat objek info window
+            var infowindow2 = new google.maps.InfoWindow({
+                content: "<h2>Hello Bali!</h2>",
+                position: data2
+            });
+
+            // membua
+            var marker2 = new google.maps.Marker({
+                position: data2,
+                map: mapreposisi,
+                title: 'Pulau Bali'
+            });
+
+            marker2.addListener('click', function() {
+                // tampilkan info window di atas marker
+                infowindow2.open(mapreposisi, marker2);
+            });
+
+
+            //// DATA 3 //
+            var data3 = {
+                lat: -7.913809383221543,
+                lng: 115.78799589736425
+            };
+
+
+            // membuat objek info window
+            var infowindow3 = new google.maps.InfoWindow({
+                content: "<h2>Hello Bali!</h2>",
+                position: data3
+            });
+            // membua
+
+            var marker3 = new google.maps.Marker({
+                position: data3,
+                map: mapreposisi,
+                title: 'Pulau Bali'
+            });
+
+            marker3.addListener('click', function() {
+                // tampilkan info window di atas marker
+                infowindow3.open(mapreposisi, marker3);
+            });
+
+
+            const flightPlanCoordinates = [{
+                    lat: -8.35325789882126,
+                    lng: 115.24945007665592
+                },
+                {
+                    lat: -7.913809383221543,
+                    lng: 115.78799589736425
+                },
+
+            ];
+            const flightPath = new google.maps.Polyline({
+                path: flightPlanCoordinates,
+                geodesic: true,
+                strokeColor: "#FF0000",
+                strokeOpacity: 1.0,
+                strokeWeight: 2,
+            });
+
+            flightPath.setMap(mapreposisi);
+
+        }
+    </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCN9D3kjoY6amHei1ZDeNIdNSLlY6kbitQ&callback=initMap">
+    </script>
+</body>
+
+</html>
---
+++ markerjson/index.php
@@ -0,0 +1,109 @@
+<html>
+
+<head>
+    <title>Markers</title>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+    <style type="text/css">
+        #map {
+            height: 100%;
+        }
+
+        /*
+         * Optional: Makes the sample page fill the window.
+         */
+        html,
+        body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div id="map"></div>
+
+    <script type="text/javascript">
+        let map;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 10,
+                center: new google.maps.LatLng(-4.926373265240449, 105.01915962173678),
+                mapTypeId: "terrain",
+            });
+
+            // Create a <script> tag and set the USGS URL as the source.
+            const script = document.createElement("script");
+
+            // This example uses a local copy of the GeoJSON stored at
+            // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+            script.src =
+                "http://localhost/yoservices/api/reposisi/maps/4";
+            document.getElementsByTagName("head")[0].appendChild(script);
+        }
+
+        // Loop through the results array and place a marker for each
+        // set of coordinates.
+        const eqfeed_callback = function(results) {
+
+            var flightPlanCoordinates = [];
+            for (let i = 0; i < results.features.length; i++) {
+
+                const coords = results.features[i].geometry.coordinates;
+                
+                var longitudes = parseFloat(coords[0]);
+                var latitudes = parseFloat(coords[1]);
+                const latLng = new google.maps.LatLng(latitudes, longitudes);
+
+
+
+
+                marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map,
+                });
+
+                const contentString = results.features[i].properties.jam;
+
+                infoWindow = new google.maps.InfoWindow({
+                    content: contentString,
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    infoWindow.open(map, marker);
+                });
+
+                google.maps.event.trigger(marker, 'click');
+
+
+
+                flightPlanCoordinates.push({
+                    lat: latitudes,
+                    lng: longitudes
+                });
+
+            }
+
+
+
+            const flightPath = new google.maps.Polyline({
+                path: flightPlanCoordinates,
+                geodesic: true,
+                strokeColor: "#FF0000",
+                strokeOpacity: 1.0,
+                strokeWeight: 2,
+            });
+
+            flightPath.setMap(map);
+
+
+
+        };
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCN9D3kjoY6amHei1ZDeNIdNSLlY6kbitQ&callback=initMap&v=weekly" defer></script>
+</body>
+
+</html>
